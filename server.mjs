03c022c949587f7ade0fce314e86ab7ec0657786
Victor Willian <victorwillian1012@gmail.com>
2022-06-12 13:01:50 -0300

    simple page with conditions (pages / urls)

--- server.mjs
+++ server.mjs
@@ -1,8 +1,8 @@
 import { createServer } from "http";
 
 createServer((req, res) => {
-    res.write("ola");
-    res.end();
+    if (req.url === "/") return res.end("<h1> Home page </h1>");
+    if (req.url === "/about") return res.end("<h1> About </h1>");
 }).listen(5000, () => {
     console.log("Server is running 🌟");
 });
