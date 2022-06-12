8efa87788049cf2f9137fdfe26750941e3645a5a
Victor Willian <victorwillian1012@gmail.com>
2022-06-12 13:31:38 -0300

    add package e nodemon

--- server.mjs
+++ server.mjs
@@ -7,15 +7,18 @@ const fileName = fileURLToPath(import.meta.url);
 const dirName = path.dirname(fileName);
 
 createServer((req, res) => {
-    if (req.url === "/") {
-        fs.readFile(
-            path.join(dirName, "public", "/index.html"),
-            (error, content) => {
-                if (error) throw error;
-                res.end(content);
-            }
-        );
-    }
+    const file = req.url === "/" ? "index.html" : req.url;
+    const filePath = path.join(dirName, "public", file);
+    const extName = path.extname(filePath);
+
+    const allowedFileTypes = [".html", ".css", ".js"];
+    const allowed = allowedFileTypes.find((item) => item === extName);
+
+    if (!allowed) return;
+    fs.readFile(filePath, (error, content) => {
+        if (error) throw error;
+        res.end(content);
+    });
 }).listen(5000, () => {
     console.log("Server is running 🌟");
 });
