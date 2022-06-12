c4948aae7ddcaa8ae8c46b8f77f30ad8cc27b5c7
Victor Willian <victorwillian1012@gmail.com>
2022-06-12 13:13:15 -0300

    return a simple page with template

--- server.mjs
+++ server.mjs
@@ -1,8 +1,21 @@
 import { createServer } from "http";
+import fs, { readvSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileName);
 
 createServer((req, res) => {
-    if (req.url === "/") return res.end("<h1> Home page </h1>");
-    if (req.url === "/about") return res.end("<h1> About </h1>");
+    if (req.url === "/") {
+        fs.readFile(
+            path.join(dirName, "public", "/index.html"),
+            (error, content) => {
+                if (error) throw error;
+                res.end(content);
+            }
+        );
+    }
 }).listen(5000, () => {
     console.log("Server is running 🌟");
 });
